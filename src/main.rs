60012a11c8ab6ef0f2cf9fc059b4bffb8f931463
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-02-29 00:22:14 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,10 @@ fn main() {
     let toml_str = toml::to_string(&config).unwrap();
 
     // tomlディレクトリの中にtest.tomlファイルに書き込みます
-    std::fs::write("db/output/test.toml", toml_str).unwrap();
+    std::fs::write("db/test.toml", toml_str).unwrap();
 
     // tomlディレクトリの中のtest.tomlファイルを読み込みます
-    let toml_str = std::fs::read_to_string("db/output/test.toml").unwrap();
+    let toml_str = std::fs::read_to_string("db/test.toml").unwrap();
 
     // TOML形式の文字列を構造体にデシリアライズします
     let config: Config = toml::from_str(&toml_str).unwrap();
