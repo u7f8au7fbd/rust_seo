1bea8391820aa068bb2f6f8dd05c68df95438796
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-02-29 11:18:55 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tools::forming;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    forming::clean_temp(); //ファイルのクリーンアップ
+    forming::clean_temp();
     connect::get("https://www.rust-lang.org").await?;
     connect::get("https://www.google.com").await?;
     connect::get("https://www.yahoo.co.jp").await?;
--- src/tools/forming.rs
+++ src/tools/forming.rs
@@ -5,7 +5,7 @@ pub fn url_to_name(url: &str) -> String {
         .replace(['?', '!', '/'], "-")
 }
 
-pub(crate) fn clean_temp() {
+pub fn clean_temp() {
     let _ = std::fs::remove_dir_all("temp");
     let _ = std::fs::create_dir("temp");
 }
